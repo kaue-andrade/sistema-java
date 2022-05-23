c404559ff84b26c05a7dfc1fc3b481e8654b7835
kaue-andrade <kauedd22@gmail.com>
2022-05-23 18:07:12 -0300

    Sistema

--- Cantina/src/PlanoMensal.java
+++ Cantina/src/PlanoMensal.java
@@ -7,7 +7,7 @@ public class PlanoMensal {
         HashMap<String, Integer> alunos = new HashMap<String, Integer>();
 
         alunos.put("Kauê Andrade dos Santos", 8);
-        alunos.put("Ruth Oliveira", 8);
+        alunos.put("Ruth  Pereira", 8);
         alunos.put("Antonio Carlos Borges de Souza", 7);
         alunos.put("Antônio Leopoldo França Filho", 6);
         alunos.put("Ítalo Ricardo de Andrade Silva", 6);
