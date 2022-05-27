a7cb8707742f5f477d794c2655eccbb3f763c626
Kauê Andrade <91803383+kaue-andrade@users.noreply.github.com>
2022-05-27 10:48:24 -0300

    Update Funcionarios.java

--- Cantina/src/Funcionarios.java
+++ Cantina/src/Funcionarios.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Funcionarios {
+    public void bancoFuncionario() {
+        Scanner ler = new Scanner(System.in);
 
+        HashMap<Integer, String> funcionario = new HashMap<Integer, String>();
 
+        funcionario.put(1234, "Josivaldo Ferreira da Silva");
+        funcionario.put(4875, "Aberlado Souza dos Santos");
+        funcionario.put(9658, "Carlos Ferreira Andrade");
+        funcionario.put(3458, "André José da Silva Santos");
 
+        System.out.print("\nID do funcionário: ");
+        int id = ler.nextInt();
+
+        System.out.println();
+
+        System.out.println("Nome do funcionário: " + funcionario.get(id));
+
+    }
 }
